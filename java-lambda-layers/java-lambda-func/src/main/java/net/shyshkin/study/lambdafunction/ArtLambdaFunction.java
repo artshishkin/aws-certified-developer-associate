f261d0e3f1ee542a6d47931d59743af08fcf24d4
Art <d.art.shishkin@gmail.com>
2020-12-29 12:57:46 +0200

    Added logging to ArtLambdaFunction (#17, #14)

--- java-lambda-layers/java-lambda-func/src/main/java/net/shyshkin/study/lambdafunction/ArtLambdaFunction.java
+++ java-lambda-layers/java-lambda-func/src/main/java/net/shyshkin/study/lambdafunction/ArtLambdaFunction.java
@@ -1,10 +1,23 @@
 package net.shyshkin.study.lambdafunction;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import net.shyshkin.study.lambdalayer.LambdaAbstractWrapper;
+
+import java.util.List;
+import java.util.Map;
 
 public class ArtLambdaFunction extends LambdaAbstractWrapper<String> {
     @Override
     protected String processMessage(String messageBody) throws Exception {
-        return messageBody.toUpperCase()+"_OK";
+        return messageBody.toUpperCase() + "_OK";
+    }
+
+    @Override
+    public List<String> handleRequest(Map<String, Object> input, Context context) {
+        List<String> resultList = super.handleRequest(input, context);
+        LambdaLogger logger = context.getLogger();
+        resultList.forEach(logger::log);
+        return resultList;
     }
 }
