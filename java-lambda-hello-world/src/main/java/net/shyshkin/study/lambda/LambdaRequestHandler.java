7f7aba4b951b0e3d286f5e57746f35436b65142b
Art <d.art.shishkin@gmail.com>
2020-12-30 02:35:02 +0200

    migrating to S3AsyncClient - list objects from certain bucket (#17)

--- java-lambda-hello-world/src/main/java/net/shyshkin/study/lambda/LambdaRequestHandler.java
+++ java-lambda-hello-world/src/main/java/net/shyshkin/study/lambda/LambdaRequestHandler.java
@@ -1,37 +1,54 @@
 package net.shyshkin.study.lambda;
 
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.s3.AmazonS3;
-import com.amazonaws.services.s3.AmazonS3ClientBuilder;
-import com.amazonaws.services.s3.model.Bucket;
-import com.amazonaws.xray.AWSXRay;
-import com.amazonaws.xray.handlers.TracingHandler;
+import software.amazon.awssdk.services.s3.S3AsyncClient;
+import software.amazon.awssdk.services.s3.model.Bucket;
+import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
+import software.amazon.awssdk.services.s3.model.ListObjectsV2Response;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
 
 public class LambdaRequestHandler implements RequestHandler<String, String> {
 
-    AmazonS3 s3Client = AmazonS3ClientBuilder.standard()
-            .withRequestHandlers(new TracingHandler(AWSXRay.getGlobalRecorder()))
+    S3AsyncClient s3Client = S3AsyncClient.builder()
+//            . withRequestHandlers(new TracingHandler(AWSXRay.getGlobalRecorder()))
             .build();
 
     @Override
     public String handleRequest(String input, Context context) {
 
-        LambdaLogger logger = context.getLogger();
-        logger.log("Input: " + input);
-        List<Bucket> buckets = s3Client.listBuckets();
         String parallelism = System.getenv("PARALLELISM");
 //        System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism","10");
         System.setProperty("java.util.concurrent.ForkJoinPool.common.parallelism", parallelism);
 
-        buckets.parallelStream()
-                .peek(bucket -> logger.log("bucket location: " + s3Client.getBucketLocation(bucket.getName())))
-                .filter(bucket -> s3Client.getBucketLocation(bucket.getName()).equalsIgnoreCase("eu-north-1"))
-                .map(bucket -> String.format("[bucket: %s] objects count %d", bucket.getName(), s3Client.listObjectsV2(bucket.getName()).getKeyCount()))
-                .forEach(logger::log);
-        return "Hello World - " + input + " parallelism " + parallelism;
+        LambdaLogger logger = context.getLogger();
+        logger.log("Input: " + input);
+        CompletableFuture<ListBucketsResponse> listBucketsCF = s3Client.listBuckets();
+        List<String> join = listBucketsCF
+                .thenApply(listBucketsResponse -> {
+                    List<Bucket> buckets = listBucketsResponse.buckets();
+
+                    List<CompletableFuture<String>> bucketSizes = buckets
+                            .stream()
+                            .filter(bucket -> bucket.name().contains("art-kate"))
+                            .map(
+                                    bucket -> s3Client
+                                            .listObjectsV2(builder -> builder.bucket(bucket.name()))
+                                            .thenApply(ListObjectsV2Response::keyCount)
+                                            .thenApply(keyCount -> String.format("[Bucket `%s` has %d keys]", bucket.name(), keyCount))
+                                            .whenCompleteAsync((message, ex) -> logger.log(message))
+                            )
+                            .collect(Collectors.toList());
+
+                    return bucketSizes.stream().map(CompletableFuture::join).collect(Collectors.toList());
+                })
+                .join();
+
+        return "Input is - " + input + "; parallelism " + parallelism + "; Bucket objects count info: " + String.join("|", join);
     }
 }
