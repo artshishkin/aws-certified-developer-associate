d54e057eb42f50b06db429b542e7b14895432675
Art <d.art.shishkin@gmail.com>
2021-01-04 00:23:00 +0200

    refactored Helper class (#26, #15)

--- webflux-dynamodb/src/main/java/net/shyshkin/study/webfluxdynamodb/utils/Helper.java
+++ webflux-dynamodb/src/main/java/net/shyshkin/study/webfluxdynamodb/utils/Helper.java
@@ -4,38 +4,26 @@ import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
 
 import java.beans.FeatureDescriptor;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Objects;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class Helper {
-    private static String[] getNullPropertyNamesPre8 (Object source) {
-        final BeanWrapper src = new BeanWrapperImpl(source);
-        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
-
-        Set<String> emptyNames = new HashSet<String>();
-        for(java.beans.PropertyDescriptor pd : pds) {
-            Object srcValue = src.getPropertyValue(pd.getName());
-            if (srcValue == null) emptyNames.add(pd.getName());
-        }
-
-        String[] result = new String[emptyNames.size()];
-        return emptyNames.toArray(result);
-    }
 
     public static String[] getNullPropertyNames(Object source) {
-        final BeanWrapper wrappedSource = new BeanWrapperImpl(source);
-        return Stream.of(wrappedSource.getPropertyDescriptors())
-                .map(FeatureDescriptor::getName)
-                .filter(propertyName -> wrappedSource.getPropertyValue(propertyName) == null)
-                .toArray(String[]::new);
+        return getPropertyNames(source, Objects::isNull);
     }
 
     public static String[] getNonNullPropertyNames(Object source) {
+        return getPropertyNames(source, Objects::nonNull);
+    }
+
+    private static String[] getPropertyNames(Object source, Predicate<Object> predicate) {
+
         final BeanWrapper wrappedSource = new BeanWrapperImpl(source);
         return Stream.of(wrappedSource.getPropertyDescriptors())
                 .map(FeatureDescriptor::getName)
-                .filter(propertyName -> wrappedSource.getPropertyValue(propertyName) != null)
+                .filter(propertyName -> predicate.test(wrappedSource.getPropertyValue(propertyName)))
                 .toArray(String[]::new);
     }
 }
