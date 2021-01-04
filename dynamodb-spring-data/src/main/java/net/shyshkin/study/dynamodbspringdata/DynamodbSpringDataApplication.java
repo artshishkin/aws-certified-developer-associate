7fff818a62990cd7215a63bee57ac8cf954d6616
Art <d.art.shishkin@gmail.com>
2021-01-04 16:07:15 +0200

    Init Commit (#25 DynamoDB With Spring Data) #15

---
+++ dynamodb-spring-data/src/main/java/net/shyshkin/study/dynamodbspringdata/DynamodbSpringDataApplication.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.dynamodbspringdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DynamodbSpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DynamodbSpringDataApplication.class, args);
+    }
+
+}
---
+++ dynamodb-spring-data/src/test/java/net/shyshkin/study/dynamodbspringdata/DynamodbSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package net.shyshkin.study.dynamodbspringdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamodbSpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
