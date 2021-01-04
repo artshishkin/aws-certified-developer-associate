581f31e364a54ba1801c2ae798ed0882e72c81dd
Art <d.art.shishkin@gmail.com>
2021-01-04 21:03:38 +0200

    added search endpoint (#25,  #15)

--- dynamodb-spring-data/src/main/java/net/shyshkin/study/dynamodbspringdata/controller/ProductInfoController.java
+++ dynamodb-spring-data/src/main/java/net/shyshkin/study/dynamodbspringdata/controller/ProductInfoController.java
@@ -3,9 +3,11 @@ package net.shyshkin.study.dynamodbspringdata.controller;
 import lombok.RequiredArgsConstructor;
 import net.shyshkin.study.dynamodbspringdata.model.ProductInfo;
 import net.shyshkin.study.dynamodbspringdata.repository.ProductInfoRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("products")
@@ -15,7 +17,24 @@ public class ProductInfoController {
     private final ProductInfoRepository productInfoRepository;
 
     @GetMapping
-    public Iterable<ProductInfo> getAllProductInfos(){
+    public Iterable<ProductInfo> getAllProductInfos() {
         return productInfoRepository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public ProductInfo getProductInfos(@PathVariable("id") String id) {
+        return productInfoRepository
+                .findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product Info with id `" + id + "` not found"));
+    }
+
+    @GetMapping("search")
+    public List<ProductInfo> search(@RequestParam("cost") String cost) {
+        return productInfoRepository.findByCostContaining(cost);
+    }
+
+    @ExceptionHandler
+    public String handleException(Exception ex) {
+        return ex.getMessage();
     }
 }
--- dynamodb-spring-data/src/main/java/net/shyshkin/study/dynamodbspringdata/repository/ProductInfoRepository.java
+++ dynamodb-spring-data/src/main/java/net/shyshkin/study/dynamodbspringdata/repository/ProductInfoRepository.java
@@ -1,10 +1,14 @@
 package net.shyshkin.study.dynamodbspringdata.repository;
 
-
 import net.shyshkin.study.dynamodbspringdata.model.ProductInfo;
 import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 @EnableScan
 public interface ProductInfoRepository extends CrudRepository<ProductInfo, String> {
+
+    List<ProductInfo> findByCostContaining(String costPart);
+
 }
